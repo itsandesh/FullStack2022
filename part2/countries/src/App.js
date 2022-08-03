017d1998cd8afacb085b7d656f2ad905bb5b0e1e
Sandesh Khanal <khanalsandesh66@gmail.com>
2022-08-03 22:46:59 +0545

    managing the commits

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,9 +1,60 @@
+<<<<<<< HEAD
+import logo from './logo.svg';
+import './App.css';
+=======
+<<<<<<< HEAD
 const App = () => {
   return (
     <div>
+>>>>>>> a9a85616b6fdb1a5fc79aaaa8fedaaf05d5420de
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
+<<<<<<< HEAD
+  );
+=======
   )
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> ba89fef996d9c4ba185f6e5dff609de33c877bdf
+>>>>>>> a9a85616b6fdb1a5fc79aaaa8fedaaf05d5420de
 }
 
 export default App;
--- part2/countries/src/index.js
+++ part2/countries/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+=======
+<<<<<<< HEAD
 import App from './App';
 import axios from 'axios'
 
+=======
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+>>>>>>> ba89fef996d9c4ba185f6e5dff609de33c877bdf
+>>>>>>> a9a85616b6fdb1a5fc79aaaa8fedaaf05d5420de
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +22,17 @@ root.render(
     <App />
   </React.StrictMode>
 );
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a9a85616b6fdb1a5fc79aaaa8fedaaf05d5420de
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+<<<<<<< HEAD
+=======
+>>>>>>> ba89fef996d9c4ba185f6e5dff609de33c877bdf
+>>>>>>> a9a85616b6fdb1a5fc79aaaa8fedaaf05d5420de
