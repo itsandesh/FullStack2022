6fd40a93b82b26a3f6ce92f53fc3e99b903c1c8a
Sandesh Khanal <khanalsandesh66@gmail.com>
2022-08-03 21:24:09 +0545

    Exercise 2.11 Completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
-
-import { useState } from 'react';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons.js';
 import PersonForm from './components/PersonForm.js';
 import Filter from './components/Filter.js'
@@ -7,18 +7,23 @@ import Filter from './components/Filter.js'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setfilter] = useState('');
+  const [namesToShow, setNamesToShow] = useState([]);
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setfilter] = useState('')
-  const [namesToShow, setNamesToShow] = useState(persons)
-
+  useEffect(() => {
+    // console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        // console.log('promise fulfilled')
+        setPersons(response.data)
+        setNamesToShow(response.data)
+      })
+  }, [])
+  // console.log('render', persons.length, 'persons')
 
   const addContact = (event) => {
     if (persons.some((person) => person.name === newName)) {
@@ -41,15 +46,12 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
   }
   const handleSearchfilter = (event) => {
-    console.log(event.target.value)
     const search = (event.target.value)
     setfilter(search)
     setNamesToShow(
